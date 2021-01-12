1a75daccbc69d64a568c88e5c38d91a0c659b971
Dick Stenqvist <stenqvist.dick@gmail.com>
2021-01-12 09:30:42 +0100

    fix

--- server/expressApp.js
+++ server/expressApp.js
@@ -42,7 +42,7 @@ app.use(
 // Handle production
 if(process.env.NODE_ENV === 'production') {
   // static folder
-  app.use(express.static(__dirname + '/piblic'))
+  app.use(express.static(__dirname + '/public'))
 
   // Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirName + '/public/index.html'))
