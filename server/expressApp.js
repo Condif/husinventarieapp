5f02254d6de059f57f61c4ffc0a45ea056d8ebb0
Dick Stenqvist <stenqvist.dick@gmail.com>
2021-01-12 12:09:24 +0100

    test

--- server/expressApp.js
+++ server/expressApp.js
@@ -69,7 +69,7 @@ app.use(itemRouter)
 if(process.env.NODE_ENV === 'production') {
   // static folder
   app.use(express.static(__dirname + '/public'))
-console.log("testar");
+
   // Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirName + '/public/index.html'))
 }
