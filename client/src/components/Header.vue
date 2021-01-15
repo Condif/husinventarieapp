7463cc5ab9fd1ae6a427e86a6c9d593e26c1d22d
Dick Stenqvist <stenqvist.dick@gmail.com>
2021-01-15 15:31:16 +0100

    styling header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -31,11 +31,14 @@
       </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer
+    <!-- <v-navigation-drawer
       v-model="drawer"
       app
       style="background: linear-gradient(to right top, #e8dfe0, #ead3d1, #e9c7be, #e4bea8, #dab692)"
     >
+    <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
       <v-list-item>
         <v-list-item-title class="title">
           <router-link to="/createitem" exact>
@@ -50,6 +53,41 @@
           </router-link>
         </v-list-item-title>
       </v-list-item>
+    </v-navigation-drawer> -->
+
+    <v-navigation-drawer
+      class="deep-purple accent-4"
+      dark
+      v-model="drawer"
+       app
+      style="background: linear-gradient(to right top, #e8dfe0, #ead3d1, #e9c7be, #e4bea8, #dab692)"
+   
+    >
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <router-link :to="item.link" exact> 
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </router-link>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block color="accent1">
+            Logout
+          </v-btn>
+        </div>
+      </template>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -59,6 +97,13 @@ export default {
   name: "Header",
   data: () => ({
     drawer: false,
+     items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard', link: '/home'},
+          { title: 'Mitt Hus', icon: 'mdi-home-city', link: '/createitem'},
+          { title: 'Projekt', icon: 'mdi-calendar-check-outline', link: '/createitem'},
+          { title: 'Mitt konto', icon: 'mdi-account-box', link:'/createproject' },
+          { title: 'Admin', icon: 'mdi-gavel', link: '/createitem'},
+        ],
   }),
   methods: {
     async logout() {
