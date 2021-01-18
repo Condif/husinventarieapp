ce84e52405cd794665d09f713b068a142e68aa86
Dick Stenqvist <stenqvist.dick@gmail.com>
2021-01-18 15:17:37 +0100

    item

--- client/src/components/Item.vue
+++ client/src/components/Item.vue
@@ -3,31 +3,87 @@
     <Quick-start />
     <v-row>
       <v-col color="secondary">
-        <v-card-title v-if="item !== undefined">
-          {{ item.itemName}}
-        </v-card-title>
-        <v-card-title  >
-          <v-img  height="200" :src='image'></v-img>
-        </v-card-title>
-        <v-card-title v-if="item !== undefined">
-            {{ item.description}}
-        </v-card-title>
-        <v-card-title v-if="item !== undefined">
-            {{ item.orderDate}}
-        </v-card-title>
-        <v-card-title v-if="item !== undefined">
-            {{ item.warranty}}
-        </v-card-title>
-        <v-card-title v-if="item !== undefined">
-            {{ item.headerText}}
-        </v-card-title> 
-       </v-col> 
+        <v-card class="mx-auto" max-width="344" v-if="items !== undefined">
+          <v-card-text>
+            <div>Inventarie</div>
+            <p class="display-1 text--primary">
+              {{ item.itemName }}
+            </p>
+            <v-btn icon>
+            <v-icon color="grey lighten-1">mdi-open-in-new</v-icon>
+          </v-btn>
+            <v-img height="200" :src="image"></v-img>
+            <div class="text--primary">
+              {{ item.description }}
+              <p>Inköpsdatum: {{ item.orderDate }}</p>
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text color="teal accent-4" @click="reveal = true">
+              Visa mer
+            </v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <v-card
+              v-if="reveal"
+              class="transition-fast-in-fast-out v-card--reveal"
+              style="height: 100%;"
+            >
+              <v-card-text class="pb-0">
+                
+                <p>Garanti: {{ item.warranty }}</p>
+                <v-img height="200" :src="image"></v-img>
+                <p>Kvitto</p>
+                <p>Rum: {{ item.warranty }}</p>
+                <p>Projekt: {{ item.warranty }}</p>
+              </v-card-text>
+              <v-card-actions class="pt-0">
+                <v-btn text color="teal accent-4" @click="reveal = false">
+                  Stäng
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
     </v-row>
     <v-row>
       <v-col sm="6" md="5">
-        <Create-item />
+        <v-card class="mx-auto" max-width="344">
+          <v-card-text>
+           
+                  <h5>Alla inventarier</h5>
+               
+    
+    <v-list-item two-line v-for="(item, i) in items" :key="i">
+      <v-list-item-content>
+        <v-list-item-title>{{item.itemName}}</v-list-item-title>
+        <v-list-item-subtitle>
+          {{item.description}},
+        </v-list-item-subtitle>
+      
+      </v-list-item-content>
+        <v-list-item-action>
+          <v-btn icon>
+            <v-icon color="grey lighten-1">mdi-open-in-new</v-icon>
+          </v-btn>
+  
+        </v-list-item-action>
+    </v-list-item>
+     
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" >
+              Lägg till nytt inventarie
+            </v-btn>
+          </v-card-actions>
+
+          
+        </v-card>
       </v-col>
     </v-row>
+        <Create-item />
   </v-container>
 </template>
 
@@ -39,30 +95,34 @@ export default {
   namespaced: true,
   name: "item",
   data: () => ({
-    
- 
+    reveal: false,
   }),
   components: {
     QuickStart,
     CreateItem,
   },
   methods: {
-      log(message) {
-          console.log(message)
-      }
+    log(message) {
+      console.log(message);
+    },
   },
   computed: {
     items() {
       return this.$store.getters["ITEMS/getItems"];
     },
     item() {
-       
-      return this.$store.getters["ITEMS/getItemFromItems"](`${this.$store.getters["ITEMS/getItem"]}`)
-       
+      return this.$store.getters["ITEMS/getItemFromItems"](
+        `${this.$store.getters["ITEMS/getItem"]}`
+      );
     },
     image() {
-    return "api/images/" + this.$store.getters["ITEMS/getItemFromItems"](`${this.$store.getters["ITEMS/getItem"]}`).imageId
-    }
+      return (
+        "api/images/" +
+        this.$store.getters["ITEMS/getItemFromItems"](
+          `${this.$store.getters["ITEMS/getItem"]}`
+        ).imageId
+      );
+    },
   },
 };
 </script>
@@ -70,5 +130,11 @@ export default {
 <style>
 .home {
   height: 100%;
+}
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
 }
 </style>
